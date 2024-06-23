880dfe524cbae2fa1f93b003c1df673b5f0cb251
Denbert Castillo <[castillodembert@gmail.com]>
2024-06-22 22:07:03 -0500

    Conexion a API Monedas del mundo.

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        
+        Scanner lectura= new Scanner(System.in);
+        System.out.println("Escribe la moneda que deseas consultar ");
+
+        ConsultaMoneda consulta= new ConsultaMoneda();
+        try {
+            var nombreDeMoneda= lectura.nextLine();
+            Moneda moneda  = consulta.buscaMoneda(nombreDeMoneda);
+            System.out.println(moneda);
+        }catch(NumberFormatException e){
+            System.out.println("Numero no encontrado " + e.getMessage());
+        } catch (RuntimeException e ){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación");
+        }
+
     }
 }
